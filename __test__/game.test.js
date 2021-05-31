f6b1d214518427272f9aa7264717fa89978f8561
faissali <44244133+faissali@users.noreply.github.com>
2021-05-31 20:17:30 +0200

    Update game.test.js

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -1,10 +1,1 @@
-const fs = require('fs')
 
-describe('App', () => {
-  it('Contains the compiled JavaScript', async (done) => {
-    fs.readFile('./public/main.js', 'utf8', (err, data) => {
-      expect(err).toBe(null)
-      expect(data).toMatchSnapshot()
-    })
-  })
-})
